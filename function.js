448e5d73e39e4db863129d600ce0ba9bf26e0f28
Musharof Hossain <musharof.caab@gmail.com>
2025-08-12 15:18:53 +0200

    version2

--- function.js
+++ function.js
@@ -1,5 +1,31 @@
-function myFunction(p1, p2) {
-    return p1 * p2;
-}
-let result = myFunction(4, 3);
+// function myFunction(p1, p2) {
+//     return p1 * p2;
+// }
+// let result = myFunction(4, 3);
+
+
+// function toCelcius(f) {
+//     return (5/9) * (f-32);
+// }
+
+// let value = toCelcius(90);
+
+
+// function myFunction() {
+//     let carName = "Volvo";
+//     // let text = "Inside: " + typeof carName + " " + carName;
+//     carName;
+// }
+
+// let myFunction = function(a, b) {return a * b};
+
+// let result = myFunction(5, 3);
+
+
+
+myFunction = (a, b) => a * b;
+
+let result = myFunction(30, 2);
+
+console.clear();
 console.log(result);
